c0afe9d88888d5129fb568b6b0248cedb043c681
Marcos Henrique <marcossistemas.ac@gmail.com>
2022-11-24 13:33:22 -0300

    constantes CONST, USANDO  a variável

---
+++ JavaScript-course/AulasBásicas/const.js
@@ -0,0 +1,44 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos 
+// Não pode começar o nome de uma constante com um número 
+// Não podem conter espaços ou traços 
+// Utilizamos camelCase
+// Case-sensitive 
+// Não podemos modificar o valor de uma constante
+// NÃO UTILIZE VAR, UTILIZE CONST 
+
+
+//declarado valor 
+/* const nome = 'Marcos Henrique'
+console.log(nome) */
+
+//Não pode mudar nome da variável
+/* const nome2 = 'MARCOS'
+nome2 = 'Marcos' */
+
+//Se fosse let daria certo 
+/* let nome3 = 'MARCOS'
+nome3 = 'João'
+console.log(nome3) */
+
+// dar para atribuir constantes desta forma, usando-as, não mudando-as 
+// + - * / 
+// String = Texto | Number = Número 
+/* const primeiroNumero = 5 
+const segundoNumero = 10
+const resultado = primeiroNumero * segundoNumero 
+const resultadoDuplicado = resultado *2 
+let resultadoTriplicado = resultado *3 
+resultadoTriplicado = resultadoTriplicado + 5 
+console.log(resultadoTriplicado)
+console.log(primeiroNumero) */ // observe que o primeiro numero não mudou, continua sendo a mesma
+// só foi usado formas de progredir com esta variável 
+
+// tipo de dados 
+/* console.log(typeof primeiroNumero) */
+
+//Quando é string 
+/* const primeiroNumeroString = '5'
+console.log(typeof primeiroNumeroString) */
+
+// conclusão: voce pode pegar o valor de uma constante e jogar dentro de outra constante, mas não altera-la
