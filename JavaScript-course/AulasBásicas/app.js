dde023a0d39ebf74eea0054d4bfbd19b58c498a5
Marcos Henrique <marcossistemas.ac@gmail.com>
2022-11-24 13:10:52 -0300

    variáveis com let

--- JavaScript-course/AulasBásicas/app.js
+++
@@ -1,2 +0,0 @@
-//sobre o console e suas formas de exibição
-console.log(`Meu nome é "Marcos". Estou aprendendo JavaScript às`, 10.30, 'da manhã')
---
+++ JavaScript-course/AulasBásicas/index.js
@@ -0,0 +1,40 @@
+//sobre o console e suas formas de exibição
+/* console.log(`Meu nome é "Marcos". Estou aprendendo JavaScript às`, 10.30, 'da manhã') */
+
+// Variáveis 
+/* let nome = 'Marcos' // string
+console.log(nome, 'nasceu em 1996') //impressão no motor do JS */
+
+// variáveis não inicializada
+/* let name // declarou a variavél , mas não inicializou 
+console.log(name) */
+
+/* let name1
+name1 = 'Qualquer Valor'  // Inicializando a variável
+console.log(name1) */
+
+// Não podemos redeclarar variáveis 
+/* let name, or nome, or name1.  */ 
+
+// Não podemos criar variáveis com palavras reservadas. 
+/* let let
+let if 
+let console */
+
+// variáveis precisam ter nomes significativos 
+/* let n = 'Marcos'
+console.log(n) */
+
+// crie a variável com algo que faça sentido
+/* let nomeCliente = 'Marcos'
+console.log(nomeCliente)  */
+
+// não podemos iniciar variáveis com um número 
+/* let 1nome  */
+//correto
+/* let nome1  */
+
+//Não podemos usar espaços ou traços
+// Utilizamos camelCase 
+// Não podemos redeclarar variáveis com LET 
+
