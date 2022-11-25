cd3382f814d853c48e2072768ce77a603a7d05dd
Marcos Henrique <marcossistemas.ac@gmail.com>
2022-11-25 17:37:48 -0300

    Diferença entre VAR e LET

---
+++ JavaScript-course/AulasBásicas/varElet.js
@@ -0,0 +1,16 @@
+// com o VAR você pode redeclar a variavél 
+
+// aqui voce declara a variavél e usa 
+/* var nome = 'Marcos'
+console.log(nome) */
+
+//Aqui você declara e redeclara a mesma, e a que vai imprimir é a ultima imposta, ex: henrique.
+/* var nome = 'Marcos'
+var nome = 'Henrique'
+console.log(nome) */
+
+/* Usar variável assim pode dar certo, mas não é uma boa prática, e pode afetar outras variáveis do
+sistema, uma vez que fica uma variável global  */
+/* nome = 'Marcos'
+console.log(nome) */ 
+
