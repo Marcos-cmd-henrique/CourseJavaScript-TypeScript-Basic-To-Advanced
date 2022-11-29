ea83c8d9a5d335a9eee6a4efa912ac526808fe63
Marcos Henrique <marcossistemas.ac@gmail.com>
2022-11-29 14:24:50 -0300

    Alert-Confirm-Promt, exercícios

---
+++ JavaScript-course/AulasBásicas/Alert-Confirm-Prompt/Alert-Confirm-Prompt.js
@@ -0,0 +1,24 @@
+/* alert('Com a nossa mensagem') // só exibe o alerta
+confirm('Deseja salvar?') // confirma um veradeiro ou falso
+prompt('Digite seu nome') // obtem o que voce digita */
+
+//Exercício: Digite dois número usando prompt, Transforme-os em Number. 
+// Depois some-os. Apresente o resultado com alert 
+
+// 1° forma de resolução 
+
+/* let num1 = prompt('Qual o primeiro número?')
+let num2 = prompt('Qual o segundo número? ')
+num1 = Number(num1)
+num2 = Number(num2)
+let resultado = num1 + num2
+alert('O resultado é ' + resultado)
+ */
+
+// 2° forma de resolução 
+/* let num1 = prompt('Qual o primeiro número?')
+let num2 = prompt('Qual o segundo número? ')
+num1 = Number(num1)
+num2 = Number(num2)
+alert(`O resultado foi ${num1 + num2}`) */ 
+
