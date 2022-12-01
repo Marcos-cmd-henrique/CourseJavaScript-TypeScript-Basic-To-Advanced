24787fad78546c95192be035aa0522a085d23ef9
Marcos Henrique <marcossistemas.ac@gmail.com>
2022-12-01 00:24:16 -0300

    Sobre Numbers, como usa-los

---
+++ JavaScript-course/AulasBásicas/numbers.js
@@ -0,0 +1,37 @@
+/* let num1 = 1 // number
+let num2 = 2.5 // number 
+console.log(num1 + num2) */
+
+/* let num1 = 1 
+let num2 = 2.5 
+console.log(num1.toString() + num2)
+console.log(typeof num1) //number  */
+
+// num1 = num1.toString() // aqui altera o tipo de number p/ string 
+
+// toString()
+/* let num1 = 1500 
+let num2 = 2.5 
+console.log(num1.toString(2)) // ver versão binária do número  */
+
+//toFixed() 
+/* let num1 = 10.57889551255547 
+let num2 = 2.5  
+console.log(num1.toFixed(2)) // arredondou o número. Deixa 2 casas decimais. 
+ */
+
+//isinteger(), para ver se o número é inteiro, irá retornar verdadeiro ou falso 
+/* let num1 = 10 
+let num2 = 2.5 
+console.log(Number.isInteger(num1))  */
+
+//isNaN(), verifica se a conta é verdadeira. 
+/* let num1 = 10 
+let num2 = 2.5 
+let temp = num1 * 'Ola'
+console.log(Number.isNaN(temp)) */ 
+
+
+
+
+
